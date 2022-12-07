4d55aff79c5d04cae0776173990e3f99043926d0
Manish Acharya <manish24870@gmail.com>
2022-12-07 08:02:24 +0545

    Get a single book route

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -1,13 +1,13 @@
 import express from "express";
 
 import upload from "../utils/multer.js";
-import { addBook, getAllBooks, getBooksByListing } from "../controllers/bookController.js";
+import { addBook, getAllBooks, getBook } from "../controllers/bookController.js";
 import { protect } from "../middleware/auth.js";
 
 const router = express.Router();
 
 router.post("/add", protect, upload.array("images"), addBook);
 router.get("/", protect, getAllBooks);
-router.get("/byListing", protect, getBooksByListing);
+router.get("/:bookId", protect, getBook);
 
 export default router;
